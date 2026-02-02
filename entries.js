20e7cb2729b402bcc7deea183e55c1ae65b2627b
Xian Saiful <xiansaiful@gmail.com>
2026-02-02 23:40:34 +0600

    Add employee object and demonstrate object keys, values, and entries

---
+++ entries.js
@@ -0,0 +1,14 @@
+// Keys, values, entries, delete, seal, freeze
+const employee = {
+    name: 'Alice',
+    age: 30,
+    position: 'Developer'
+}
+// Getting keys, values, and entries of the object
+const objectKeys = Object.keys(employee); // This will give an array of keys
+const objectValues = Object.values(employee); // This will give an array of values
+const objectKeysAndValues = Object.entries(employee); // This will give an array of [key, value] pairs
+
+// console.log(objectKeysAndValues);
+
+
