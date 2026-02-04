075a5ef22abafcd17d61cb8c86adce57bcbe393f
Xian Saiful <xiansaiful@gmail.com>
2026-02-04 21:48:46 +0600

    Add foreach example demonstrating array iteration and manipulation

---
+++ foreach.js
@@ -0,0 +1,7 @@
+// 2-4 foreach, filter, find, and differences between them
+
+const numbers = [5, 4, 6, 43, 56];
+numbers.forEach(number => {
+    const doSUM = number * 2;
+    console.log(doSUM);
+})
