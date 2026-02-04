3dd2b8c5ecbce32c9a6fe39780387db0f8a1c3ab
Xian Saiful <xiansaiful@gmail.com>
2026-02-04 22:18:45 +0600

    Add filter example to demonstrate array filtering and logging results

--- foreach.js
+++ foreach.js
@@ -3,5 +3,9 @@
 const numbers = [5, 4, 6, 43, 56];
 numbers.forEach(number => {
     const doSUM = number * 2;
-    console.log(doSUM);
 })
+
+// Filter
+const greaterThan10 = numbers.filter(number => number > 10);
+console.log(greaterThan10);
+
