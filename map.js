e9172c1e4726e620e49583348fe6ff61901e6719
Xian Saiful <xiansaiful@gmail.com>
2026-02-04 21:40:27 +0600

    Refactor map examples for clarity and consistency

--- map.js
+++ map.js
@@ -13,4 +13,18 @@ const doubleIt = x => x * 2;
 const doubled = numbers.map(doubleIt);
 
 // Oneline loop
-const makeDouble = numbers.map(x => x * 2);
+const makeDouble = numbers.map(number => number * 2);
+
+
+// String Length
+const friends = ["Kamal", "Zamal", "Tomal", "Khomal"];
+
+const nameLengths = friends.map(friendName => friendName.length);
+const firstLetters = friends.map(name => name[3].toLowerCase());
+
+// Mulitline Map
+const friendIndex = friends.map((index) => {
+    return index
+});
+
+console.log(friendIndex);
