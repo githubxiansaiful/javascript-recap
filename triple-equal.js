d3f395d6797e3f45dad5dc5ff6a59243b7f7102e
Xian Saiful <xiansaiful@gmail.com>
2026-02-07 00:10:24 +0600

    Add example to triple-equal.js demonstrating no implicit conversion with array and string

--- triple-equal.js
+++ triple-equal.js
@@ -8,4 +8,5 @@ console.log(false == 0); // true, implicit conversion happens
 console.log(false === 0); // false, no implicit conversion  
 console.log(null == undefined); // true, implicit conversion happens
 console.log(null === undefined); // false, no implicit conversion
+console.log([5] === "5"); // false, no implicit conversion
 
