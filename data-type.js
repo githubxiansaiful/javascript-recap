dc1a1c062e45b56cd3fae94e0670c6ca7c89a1cb
Xian Saiful <xiansaiful@gmail.com>
2026-02-06 23:32:18 +0600

    Update data-type.js to include console log for name and remove redundant logs

--- data-type.js
+++ data-type.js
@@ -1,10 +1,16 @@
 // Primitive data type
 
 const data = 1544;
-
-console.log(typeof data);
-
+const name = "John Doe";
+const isActive = true;
+const pi = 3.14;
 
 // Non-primitive data type
 const data2 = [1, 2, 3, 4, 5];
-console.log(typeof data2);
+const person = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 30,
+};
+const today = new Date();
+console.log(typeof today);
--- js-core-concepts/data-type.js
+++ js-core-concepts/data-type.js
@@ -1,8 +1,9 @@
 // Primitive data type
 
 const data = 1544;
+const name = "John Doe";
 
-console.log(typeof data);
+console.log(typeof name);
 
 
 // Non-primitive data type
