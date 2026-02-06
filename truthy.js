798d8d1e8f7b9e26dc8a2494e8462de87c928be3
Xian Saiful <xiansaiful@gmail.com>
2026-02-06 23:49:28 +0600

    Add truthy.js to demonstrate different truthy and falsy values in JavaScript

---
+++ truthy.js
@@ -0,0 +1,13 @@
+// 3-3 Different Truthy and Falsy Values in JavaScript
+let data;
+data = 0; // Falsy
+data = ""; // Falsy
+data = null; // Falsy
+data = undefined; // Falsy
+data = false; // Falsy
+data = NaN; // Falsy
+data = 1; // Truthy
+data = "Hello"; // Truthy
+data = []; // Truthy
+data = {}; // Truthy
+data = function() {}; // Truthy
