dd2860d23527c64d10592e7cea7da7c67c0fe786
Xian Saiful <xiansaiful@gmail.com>
2026-02-08 23:20:46 +0600

    Add index.html and style.css for initial project structure

--- data-type.js
+++ data-type.js
@@ -1,16 +1,17 @@
 // Primitive data type
 
-const data = 1544;
-const name = "John Doe";
-const isActive = true;
-const pi = 3.14;
+const data = 1544;  // Number
+const name = "John Doe"; // String
+const isActive = true; // Boolean
+const emptyValue = null; // undefined is a primitive data type that represents the absence of a value or an uninitialized variable.
+const pi = 3.14; // Symbol is a unique and immutable primitive data type that can be used as an identifier for object properties.
 
 // Non-primitive data type
-const data2 = [1, 2, 3, 4, 5];
-const person = {
+const data2 = [1, 2, 3, 4, 5]; // Array is a non-primitive data type that can hold multiple values of different types.
+const person = { 
   firstName: "Jane",
   lastName: "Doe",
   age: 30,
 };
-const today = new Date();
-console.log(typeof today);
+const today = new Date(); // Object is a non-primitive data type that can hold multiple values in the form of properties and methods.
+console.log(typeof today); // Output: "object"
